dfeaeef7a1b4041f93b2d3e032630894dcf6578c
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-14 10:35:51 +0800

    Add basic layout design

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,13 +12,33 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	titleText := widget.NewLabel("Newton")
+	subTitleText := widget.NewLabel("Newton's Method of Approximation Visualizer")
+	titleContainer := container.NewVBox(titleText, subTitleText)
 
-	w.ShowAndRun()
+	inputText := widget.NewLabel("Enter the function to visualize")
+	inputEntry := widget.NewEntry()
+	inputEntry.SetPlaceHolder("Function...")
+	startingPointText := widget.NewLabel("Enter the starting x-coordinate")
+	startingPointEntry := widget.NewEntry()
+	startingPointEntry.SetPlaceHolder("x-coordinate...")
+	startVisualizationButton := widget.NewButton("Start", func() {
+	})
+	stopVisualizationButton := widget.NewButton("Stop", func() {
+
+	})
+	inputContainer := container.NewVBox(
+		container.NewHBox(inputText, inputEntry),
+		container.NewHBox(startingPointText, startingPointEntry),
+		container.NewHBox(startVisualizationButton, stopVisualizationButton),
+	)
+
+	visualization := canvas.NewImageFromResource(theme.FyneLogo())
+	visualization.FillMode = canvas.ImageFillOriginal
+
+	w.SetContent(container.NewBorder(titleContainer, nil, nil, inputContainer, visualization))
+
+	w.Show()
+
+	a.Run()
 }
