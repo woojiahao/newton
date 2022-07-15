b803693800d5d7a8f81bdf6b7089e1fc547dd6b6
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-15 21:10:17 +0800

    Create ASTNode struct
    Setup struct with accompanying helper functions for quick creation of
    ASTNodes of various types

---
+++ internal/parser/ast_node.go
@@ -0,0 +1,36 @@
+package parser
+
+type ASTNodeType int
+
+const (
+	Undefined ASTNodeType = iota
+	OperatorPlus
+	OperatorMinus
+	OperatorMul
+	OperatorDiv
+	UnaryMinus
+	NumberValue
+)
+
+type ASTNode struct {
+	astNodeType ASTNodeType
+	value       Value
+	left        *ASTNode
+	right       *ASTNode
+}
+
+func emptyASTNode() *ASTNode {
+	return &ASTNode{Undefined, 0.0, nil, nil}
+}
+
+func createASTNode(astNodeType ASTNodeType, left, right *ASTNode) *ASTNode {
+	return &ASTNode{astNodeType, 0.0, left, right}
+}
+
+func createUnaryASTNode(left *ASTNode) *ASTNode {
+	return &ASTNode{UnaryMinus, 0.0, left, nil}
+}
+
+func createNumberASTNode(value Value) *ASTNode {
+	return &ASTNode{NumberValue, value, nil, nil}
+}
